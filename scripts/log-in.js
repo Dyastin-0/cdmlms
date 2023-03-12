59cd99df5a14f56a1281d85dc30344a8aa8e6b24
= <=>
2023-03-12 22:21:54 +0800

    Finished the authentication system

--- scripts/log-in.js
+++ scripts/log-in.js
@@ -1,4 +1,6 @@
-import { isUsernameAndPasswordMatched, toSha256, warning } from './validation.js';
+import { isUsernameAndPasswordMatched, generateToken, warning } from './validation.js';
+import { user } from './user.js';
+
 
 const overlay = document.getElementById("overlay");
 const modal = document.getElementById("log-in-modal");
@@ -9,7 +11,8 @@ const username = form.querySelector("#log-in-username");
 const password = form.querySelector("#log-in-password");
 const submit = modal.querySelector("#log-in-account-button");
 
-bindEvents(); 
+user.redirect();
+bindEvents();
 
 function bindEvents() {
     open.addEventListener('click', () => show());
@@ -29,14 +32,26 @@ function hide() {
     form.reset();
     warning("", "red", "log-in");
 }
+
 async function logIn() {
     const response = await isUsernameAndPasswordMatched(username.value,
     password.value);
     if (!response.error) {
-        localStorage.setItem(toSha256("data"), response.data.username);
+        const token = await generateToken(response.data);
+        await saveToken(token);
+        localStorage.setItem("session", JSON.stringify(token));
         window.location.href = './home.html';
         hide();
     } else {
         warning(response.error, "red", "log-in");
     }
+}
+
+async function saveToken(token) {
+    await db.collection('authTokens')
+    .doc(crypto.randomUUID())
+    .set(token)
+    .catch(err => {
+        console.log(err)
+    })
 }
